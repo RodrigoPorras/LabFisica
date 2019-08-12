eb90ba9d194e89a82d2edd5eec3197dd8a8b4997
Rodrigo Porras <rodrigo.porrras@udea.edu.co>
2019-08-12 15:26:04 -0500

    First and last commit

---
+++ Assets/Aceleration.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Aceleration : MonoBehaviour {
+	public float speed = 10.0F;
+	public Text console,maxAccelerationConsole;
+	public float maxAceleration = 0;
+
+	void Update() {
+		
+		console.text = "La aceleracion es :"+Input.acceleration.magnitude+" y la rotacion en x es :" + DeviceRotation.Get().eulerAngles.x;
+		if (Input.acceleration.magnitude > maxAceleration) {
+			maxAceleration = Input.acceleration.magnitude;
+			maxAccelerationConsole.text = "Aceleracion maxima = " + maxAceleration;
+		}
+
+	}
+
+	public float GetRotation(){
+		Quaternion referenceRotation = Quaternion.identity;
+		Quaternion deviceRotation = DeviceRotation.Get();
+		Quaternion eliminationOfXY = Quaternion.Inverse(
+			Quaternion.FromToRotation(referenceRotation * Vector3.forward, 
+				deviceRotation * Vector3.forward)
+		);
+		Quaternion rotationZ = eliminationOfXY * deviceRotation;
+		float roll = rotationZ.eulerAngles.z;
+		return roll;
+	}
+
+	public void ResetMaxAcceleration(){
+		maxAceleration = 0;
+		maxAccelerationConsole.text = "La aceleracion es : 0";
+	}
+}
